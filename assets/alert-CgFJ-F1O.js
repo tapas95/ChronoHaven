fdbcabb04437f170bbca0ddee6adcbbedfe958de
Tapas <tapas.paul@codeclouds.in>
2025-06-24 13:40:49 +0530

    Updates

--- assets/alert-CgFJ-F1O.js
+++
@@ -1,6 +0,0 @@
-function c(s,t="danger",r,e){return`
-        <div class="alert fs-sm lh-1 d-flex align-items-center gap-2 fw-medium mt-2 ${(n=>{switch(n){case"danger":return"bg-danger-subtle text-danger";case"warning":return"bg-warning-subtle text-warning";case"success":return"bg-success-subtle text-success";default:return""}})(t)} ${e||""}">
-            <i class="bi ${r}"></i>
-            <span class="d-block">${s}</span>
-        </div>
-    `}export{c as d};
--- assets/main-8YAdH-Aq.js
+++ assets/main-8YAdH-Aq.js
@@ -1,9 +1,9 @@
-import{c as d,d as i,g as p,o as v,a as g,b as u,e as c,s as f}from"./firebase-config-Bi0d6Smg.js";const b="/ChronoHaven/assets/chronoHaven-BAlQTs5Z.svg";function n(a,l,s){document.getElementById("app").insertAdjacentHTML("afterbegin",`
+import{c as g,d as c,g as p,o as u,a as r,b as m,e as h,f as v,s as b}from"./alert-DD8ChQue.js";const y="/ChronoHaven/assets/chronoHaven-BAlQTs5Z.svg";function o(a,t,e){document.getElementById("app").insertAdjacentHTML("afterbegin",`
         <header id="mainHeader">
             <div class="container">
                 <nav class="navbar navbar-expand-lg py-4 gap-5">
                     <a class="navbar-brand p-0" href="./">
-                        <img src="${b}" class="img-fluid d-block" />
+                        <img src="${y}" class="img-fluid d-block" />
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -38,7 +38,7 @@ import{c as d,d as i,g as p,o as v,a as g,b as u,e as c,s as f}from"./firebase-c
                         ${a?`<li class="nav-item dropdown">
                                 <a href="javascript: void(0);" class="nav-link fs-lg lh-1 dropdown-toggle d-flex align-items-center gap-2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     <i class="bi bi-person-circle"></i>
-                                    <span class="d-block">Hi, ${l}</span>
+                                    <span class="d-block">Hi, ${t}</span>
                                 </a>
                                 <ul class="dropdown-menu">
                                     <li><a class="dropdown-item" href="javascript: void(0);">My Profile</a></li>
@@ -51,4 +51,15 @@ import{c as d,d as i,g as p,o as v,a as g,b as u,e as c,s as f}from"./firebase-c
                 </nav>
             </div>
         </header>
-    `);const o=document.getElementById("logOut");o&&typeof s=="function"&&o.addEventListener("click",r=>{r.preventDefault(),s()})}const m=async()=>{document.getElementById("app");try{const a=d(i,"collections","products","items"),s=(await p(a)).docs.map(e=>e.data());s.length!==0&&s.forEach(e=>{})}catch(a){console.log(a)}},t=async()=>{try{await f(c),n(!1,null,t),window.location.href="login.html"}catch(a){console.log(a)}};v(c,async a=>{if(a)try{const l=g(i,"users",a.uid),s=await u(l);if(s.exists()){const e=s.data();n(!0,e.firstName,t)}else console.log("No such document!")}catch(l){console.log(l),n(!1,null,t)}else n(!1,null,t)});m();
+    `);const n=document.getElementById("logOut");n&&typeof e=="function"&&n.addEventListener("click",f=>{f.preventDefault(),e()})}const j=async()=>{document.getElementById("app");try{const a=g(c,"collections","products","items"),e=(await p(a)).docs.map(s=>s.data());e.length!==0&&e.forEach(s=>{})}catch(a){console.log(a)}},l=async()=>{try{await b(v),o(!1,null,l),window.location.href="login.html"}catch(a){console.log(a)}};u(v,async a=>{if(a)try{const t=m(c,"users",a.uid),e=await h(t);if(e.exists()){const s=e.data();o(!0,s.firstName,l)}else console.log("No such document!")}catch(t){console.log(t),o(!1,null,l)}else o(!1,null,l)});j();const i=document.getElementById("categories"),d=document.getElementById("categoryPlaceholder"),k=async()=>{try{const a=g(c,"collections","categories","items"),t=await p(a);t.empty&&i.insertAdjacentHTML("beforeend",r("No categories found.","danger")),t.forEach(e=>{const s=e.data(),n=`
+        <div class="col" id="${s.id}">
+          <div class="category-content">
+            <div class="category-icon bg-light px-4 py-5 mb-3 transition-3">
+              <a href="javascript: void(0);">
+                <img src="${s.icon}" alt="${s.name}" class="img-fluid" />
+              </a>
+            </div>
+            <p class="fw-semibold"><a href="javascript: void(0);" class="text-decoration-none">${s.name}</a></p>
+          </div>
+        </div>
+      `;i.insertAdjacentHTML("beforeend",n)})}catch(a){console.log("Error fetching categories:",a.code||a.message),i.insertAdjacentHTML("beforeend",r("Error loading categories.","danger"))}finally{d&&d.remove()}};k();
