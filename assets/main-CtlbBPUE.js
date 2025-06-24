86195392a51ef04a33dc016ce03b595408dfbdc5
Tapas <tapas.paul@codeclouds.in>
2025-06-24 09:05:32 +0530

    Updates

--- assets/main-CtlbBPUE.js
+++ assets/main-CtlbBPUE.js
@@ -1,9 +1,9 @@
-import{c as d,d as o,g as p,o as v,a as g,b as u,e as c,s as f}from"./firebase-config-Bi0d6Smg.js";function n(a,l,s){document.getElementById("app").insertAdjacentHTML("afterbegin",`
+import{c as d,d as i,g as p,o as v,a as g,b as u,e as c,s as f}from"./firebase-config-Bi0d6Smg.js";const b="/ChronoHaven/assets/chronoHaven-BAlQTs5Z.svg";function n(a,l,s){document.getElementById("app").insertAdjacentHTML("afterbegin",`
         <header id="mainHeader">
             <div class="container">
                 <nav class="navbar navbar-expand-lg py-4 gap-5">
                     <a class="navbar-brand p-0" href="./">
-                        <img src="./src/assets/images/chronoHaven.svg" class="img-fluid d-block" />
+                        <img src="${b}" class="img-fluid d-block" />
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -51,4 +51,4 @@ import{c as d,d as o,g as p,o as v,a as g,b as u,e as c,s as f}from"./firebase-c
                 </nav>
             </div>
         </header>
-    `);const i=document.getElementById("logOut");i&&typeof s=="function"&&i.addEventListener("click",r=>{r.preventDefault(),s()})}const m=async()=>{document.getElementById("app");try{const a=d(o,"collections","products","items"),s=(await p(a)).docs.map(e=>e.data());s.length!==0&&s.forEach(e=>{})}catch(a){console.log(a)}},t=async()=>{try{await f(c),n(!1,null,t),window.location.href="login.html"}catch(a){console.log(a)}};v(c,async a=>{if(a)try{const l=g(o,"users",a.uid),s=await u(l);if(s.exists()){const e=s.data();n(!0,e.firstName,t)}else console.log("No such document!")}catch(l){console.log(l),n(!1,null,t)}else n(!1,null,t)});m();
+    `);const o=document.getElementById("logOut");o&&typeof s=="function"&&o.addEventListener("click",r=>{r.preventDefault(),s()})}const m=async()=>{document.getElementById("app");try{const a=d(i,"collections","products","items"),s=(await p(a)).docs.map(e=>e.data());s.length!==0&&s.forEach(e=>{})}catch(a){console.log(a)}},t=async()=>{try{await f(c),n(!1,null,t),window.location.href="login.html"}catch(a){console.log(a)}};v(c,async a=>{if(a)try{const l=g(i,"users",a.uid),s=await u(l);if(s.exists()){const e=s.data();n(!0,e.firstName,t)}else console.log("No such document!")}catch(l){console.log(l),n(!1,null,t)}else n(!1,null,t)});m();
