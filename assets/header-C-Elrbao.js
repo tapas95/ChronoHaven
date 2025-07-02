f143c131b3417830480a00b1f91fb048a454693e
Tapas <tapas.paul@codeclouds.in>
2025-07-02 13:40:06 +0530

    Updates

--- assets/header-C-Elrbao.js
+++ assets/header-C-Elrbao.js
@@ -1,9 +1,9 @@
-import{o as r,h as c,d,k as g,a as l,l as h}from"./firebase-config-t6koYE1i.js";const n=document.getElementById("app"),a=document.getElementById("currentUser"),t=document.getElementById("mainHeader"),p=t.clientHeight;n&&(n.style.paddingTop=`${p}px`);t&&t.classList.add("position-fixed","top-0","end-0","start-0","z-3");r(l,async s=>{if(s)try{const e=c(d,"users",s.uid),i=await g(e);if(i.exists()){const o=i.data();a.innerHTML=`
+import{o as c,a as r,h as d,d as g,k as p,l as h}from"./firebase-config-t6koYE1i.js";const m=()=>new Promise(s=>{c(r,async e=>{s(e)})}),n=document.getElementById("app"),o=document.getElementById("currentUser"),i=document.getElementById("mainHeader"),f=i.clientHeight;n&&(n.style.paddingTop=`${f}px`);i&&i.classList.add("position-fixed","top-0","end-0","start-0","z-3");const u=async()=>{const s=await m();if(s)try{const e=d(g,"users",s.uid),t=await p(e);if(t.exists()){const l=t.data();o.innerHTML=`
                     <a href="javascript: void(0);" id="myAccount" class="nav-link dropdown-toggle d-flex align-items-center gap-2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <svg width="24" height="24" class="d-block flex-shrink-0">
                             <use href="./src/assets/images/sprite.svg#user" />
                         </svg>
-                        <span class="d-block">Hi, ${o.firstName}</span>
+                        <span class="d-block">Hi, ${l.firstName}</span>
                         <svg width="14" height="14" class="d-block flex-shrink-0">
                             <use href="./src/assets/images/sprite.svg#chevronDown" />
                         </svg>
@@ -42,18 +42,11 @@ import{o as r,h as c,d,k as g,a as l,l as h}from"./firebase-config-t6koYE1i.js";
                             </a>
                         </li>
                     </ul>
-                `,f()}else console.log("No such document!")}catch(e){console.log(e),a.innerHTML=`
-                <a href="./login.html" id="myAccount" class="nav-link d-flex align-items-center gap-2">
-                    <svg width="24" height="24" class="d-block flex-shrink-0">
-                        <use href="./src/assets/images/sprite.svg#login" />
-                    </svg>
-                    <span class="d-block">Login</span>
-                </a>
-            `}else a.innerHTML=`
-            <a href="./login.html" id="myAccount" class="nav-link d-flex align-items-center gap-2">
-                <svg width="24" height="24" class="d-block flex-shrink-0">
-                    <use href="./src/assets/images/sprite.svg#login" />
-                </svg>
-                <span class="d-block">Login</span>
-            </a>
-        `});const m=async()=>{try{await h(l),window.location.href="./login.html"}catch(s){console.log(s)}},f=()=>{document.getElementById("logOut").addEventListener("click",e=>{e.preventDefault(),m()})};
+                `,v()}else console.log("User Document Not Found!"),a()}catch(e){console.log(e),a()}else a()},a=()=>{o.innerHTML=`
+        <a href="./login.html" id="myAccount" class="nav-link d-flex align-items-center gap-2">
+            <svg width="24" height="24" class="d-block flex-shrink-0">
+                <use href="./src/assets/images/sprite.svg#login" />
+            </svg>
+            <span class="d-block">Login</span>
+        </a>
+    `},v=()=>{const s=document.getElementById("logOut");s&&s.addEventListener("click",async e=>{e.preventDefault();try{await h(r),window.location.href="./login.html"}catch(t){console.log(t)}})};u();
