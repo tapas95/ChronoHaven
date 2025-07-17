4ba85e4762ad482625419fc93c7d5fb966de0b47
Tapas <tapas.paul@codeclouds.in>
2025-07-17 16:42:04 +0530

    Updates

--- assets/thankYou-mEDA0Bfi.js
+++
@@ -1,1 +0,0 @@
-import"./firebase-config-BeTxBtPS.js";import"./header-C7f_UmUr.js";const e=document.getElementById("orderId"),o=new URLSearchParams(window.location.search),r=o.get("orderId");e&&(e.textContent=r);
---
+++ assets/thankYou-yltwIAD0.js
@@ -0,0 +1,1 @@
+import"./firebase-config-C_Yz0m2T.js";import"./header-0P7B0Tyh.js";const e=document.getElementById("orderId"),o=new URLSearchParams(window.location.search),r=o.get("orderId");e&&(e.textContent=r);
