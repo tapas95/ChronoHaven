eb5d8ba1c3d8b3c2e071d8c0bf5d0c9020d3f88e
Tapas <tapas.paul@codeclouds.in>
2025-07-17 13:01:24 +0530

    Updates

---
+++ assets/orders-BMnbCor9.js
@@ -0,0 +1,1 @@
+import"./firebase-config-BeTxBtPS.js";import{h as n}from"./header-C7f_UmUr.js";const t=document.getElementById("logOutBtn");t&&t.addEventListener("click",e=>{e.preventDefault(),n()});
---
+++ assets/thankYou-mEDA0Bfi.js
@@ -0,0 +1,1 @@
+import"./firebase-config-BeTxBtPS.js";import"./header-C7f_UmUr.js";const e=document.getElementById("orderId"),o=new URLSearchParams(window.location.search),r=o.get("orderId");e&&(e.textContent=r);
