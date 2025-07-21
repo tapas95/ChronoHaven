04316f873a4705eabf87d9345522334c8cb9f15b
Tapas <tapas.paul@codeclouds.in>
2025-07-21 12:25:46 +0530

    Updates

---
+++ assets/thankYou-cyw3yGvV.js
@@ -0,0 +1,1 @@
+import"./alert-Dd-XNGtX.js";import"./header-Ckz7qYH9.js";const e=document.getElementById("orderId"),o=new URLSearchParams(window.location.search),r=o.get("orderId");e&&(e.textContent=r);
