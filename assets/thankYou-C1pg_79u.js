89e9a69463f395d41f081cdd9597cdc458797d2f
Tapas <tapas.paul@codeclouds.in>
2025-07-21 13:06:43 +0530

    Updates

---
+++ assets/thankYou-C1pg_79u.js
@@ -0,0 +1,1 @@
+import"./alert-DWjb4CXO.js";import"./header-C8m5pGcf.js";const e=document.getElementById("orderId"),o=new URLSearchParams(window.location.search),r=o.get("orderId");e&&(e.textContent=r);
--- assets/thankYou-cyw3yGvV.js
+++
@@ -1,1 +0,0 @@
-import"./alert-Dd-XNGtX.js";import"./header-Ckz7qYH9.js";const e=document.getElementById("orderId"),o=new URLSearchParams(window.location.search),r=o.get("orderId");e&&(e.textContent=r);
