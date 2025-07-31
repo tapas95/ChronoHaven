aa56f764d21cf2167ae3a0971992f2a7a62cb661
Tapas <tapas.paul@codeclouds.in>
2025-07-31 18:30:24 +0530

    Updates

---
+++ assets/main-Dxwt8mtM.js
@@ -0,0 +1,41 @@
+import{c as w,d as x,q as F,w as I,g as C,a as c}from"./alert-DMWuZ5g4.js";import{u as q}from"./header-_iNkTwr9.js";import{r as $,a as j,c as E,b as D,S as B}from"./checkIsFavorite-CDQXA8O7.js";import{a as _}from"./addToCart-DqmwY0nF.js";const N=(t,o)=>{const u=typeof t=="string"?document.querySelector(t):t;if(!u)return;const s=()=>`
+        <div class="col">
+            <div class="category-content">
+                <span class="placeholder category-icon d-block mb-3" style="height: 12.25rem;"></span>
+                <span class="placeholder col-7" style="height: 1.125rem;"></span>
+            </div>
+        </div>
+    `,d=document.createElement("div");d.className="row d-flex px-0 mx-0",d.id="categoryCardSkeleton";for(let g=0;g<o;g++)d.insertAdjacentHTML("beforeend",s());u.appendChild(d)},R=async t=>{$(t,3);try{const o=w(x,"collections","products","items"),u=F(o,I("isFeatured","==",!0)),s=await C(u);if(s.empty){t.insertAdjacentHTML("beforeend",c("No Products Found.","danger"));return}s.forEach(d=>{const g=d.data();t.insertAdjacentHTML("beforeend",j(g))}),requestAnimationFrame(()=>{(async()=>{const d=document.querySelectorAll(".variant-images"),g=document.querySelectorAll(".product-title"),L=document.querySelectorAll(".add-to-cart"),y=document.querySelectorAll(".add-to-favorite");if(L.forEach(e=>{e.addEventListener("click",i=>{i.preventDefault(),document.querySelectorAll(".alert").forEach(a=>a.remove()),e.disabled=!0,e.insertAdjacentHTML("beforeend",'<div class="spinner-border spinner-border-sm text-light" role="status"><span class="visually-hidden">Loading...</span></div>');const p=e.getAttribute("data-product-id"),n=e.getAttribute("data-variant-id");p&&n?_(p,n,1).then(a=>{a==="ADDED"?(e.parentElement.insertAdjacentHTML("afterend",c("Product Added To Cart","success","bi-check-circle-fill","fs-xs")),q()):a==="EXISTS"&&e.parentElement.insertAdjacentHTML("afterend",c("Product Already Exist","danger","bi-exclamation-diamond-fill","fs-xs"))}).catch(a=>{console.log("Failed to add to cart",a)}).finally(()=>{e.disabled=!1;const a=e.querySelector(".spinner-border");a&&a.remove()}):console.warn("Product or Variant ID missing")})}),y)for(const e of y){const i=e.getAttribute("data-product-id"),p=e.getAttribute("data-variant-id");try{await E(i,p)&&(e.classList.add("bg-primary","text-white"),e.innerHTML=`
+                                        <svg width="18" height="18" class="d-block flex-shrink-0">
+                                            <use href="./src/assets/images/sprite.svg#heartFill"></use>
+                                        </svg>
+                                    `)}catch(n){console.log(n)}e.addEventListener("click",async n=>{n.preventDefault(),document.querySelectorAll(".alert").forEach(r=>r.remove()),e.disabled=!0,e.innerHTML='<div class="spinner-border spinner-border-sm text-primary" role="status"><span class="visually-hidden">Loading...</span></div>';const h=e.getAttribute("data-product-id"),a=e.getAttribute("data-variant-id");try{const r=await D({productId:h,variantId:a});r==="ADDED"?(e.parentElement.insertAdjacentHTML("afterend",c("Product Added To Favorites","success")),e.classList.add("bg-primary","text-white"),e.innerHTML=`
+                                            <svg width="18" height="18" class="d-block flex-shrink-0">
+                                                <use href="./src/assets/images/sprite.svg#heartFill"></use>
+                                            </svg>
+                                        `):r==="EXISTS"&&(e.parentElement.insertAdjacentHTML("afterend",c("Product Already Added To Favorites","danger")),e.innerHTML=`
+                                            <svg width="18" height="18" class="d-block flex-shrink-0">
+                                                <use href="./src/assets/images/sprite.svg#heartFill"></use>
+                                            </svg>
+                                        `)}catch(r){console.log(r)}finally{e.disabled=!1}})}d.forEach((e,i)=>{var a;new B(e,{perPage:1,arrows:!1,pagination:!0,drag:!1}).mount();const n=(a=s.docs[i])==null?void 0:a.data();if(!n||!n.variants)return;const h=e.querySelectorAll(".splide__pagination__page");n.variants.forEach((r,H)=>{const v=h[H];if(!v||!r.colors)return;const M=r.colors.map(l=>r.colors.length>1?`${l} ${100/r.colors.length}%`:l).join(", ");v.style.cssText=`
+                            --variant-base-color: ${r.colors[0]};
+                            background-image: linear-gradient( -45deg, ${M} );
+                            box-shadow: 0 0 0 0.125rem ${r.colors[0]};
+                        `,v.setAttribute("data-variant-id",`${r.id}`),v.addEventListener("click",()=>{const l=r.id,b=g[i],S=L[i],f=y[i];if(!b||!S)return;const A=new URL(b.href,window.location.origin);A.searchParams.delete("variantId"),A.searchParams.append("variantId",l),b.href=A.toString(),S.setAttribute("data-variant-id",`${l}`),f.setAttribute("data-variant-id",`${l}`);const P=f.getAttribute("data-product-id");E(P,l).then(T=>{T?(f.classList.add("bg-primary","text-white"),f.innerHTML=`
+                                        <svg width="18" height="18" class="d-block flex-shrink-0">
+                                            <use href="./src/assets/images/sprite.svg#heartFill"></use>
+                                        </svg>
+                                    `):(f.classList.remove("bg-primary","text-white"),f.innerHTML=`
+                                        <svg width="18" height="18" class="d-block flex-shrink-0">
+                                            <use href="./src/assets/images/sprite.svg#heart"></use>
+                                        </svg>
+                                    `)}).catch(T=>{console.log(T)})})})})})()})}catch(o){console.log(o),t.insertAdjacentHTML("beforeend",c("Error Loading Products.","danger"))}finally{const o=document.getElementById("productCardSkeleton");o&&o.remove()}},m=document.getElementById("categories"),V=async()=>{N(m,5);try{const t=w(x,"collections","categories","items"),o=await C(t);o.empty&&m.insertAdjacentHTML("beforeend",c("No categories found.","danger")),o.forEach(u=>{const s=u.data(),d=`
+                <div class="col" id="${s.id}">
+                    <div class="category-content">
+                        <a href="./shop.html?category=${s.id}" class="category-icon d-block bg-light px-4 py-5 mb-3 transition-3">
+                            <img src="${s.icon}" alt="${s.name}" class="img-fluid" />
+                        </a>
+                        <p class="fs-md fw-semibold"><a href="./shop.html?category=${s.id}">${s.name}</a></p>
+                    </div>
+                </div>
+            `;m&&m.insertAdjacentHTML("beforeend",d)})}catch(t){console.log("Error fetching categories:",t.code||t.message),m.insertAdjacentHTML("beforeend",c("Error loading categories.","danger"))}finally{const t=document.getElementById("categoryCardSkeleton");t&&t.remove()}};V();const k=document.getElementById("featuredProducts");k&&R(k);
---
+++ assets/shop-DE-2aPMS.js
@@ -0,0 +1,104 @@
+import{c as I,d as H,g as M,a as u,q,w as D}from"./alert-DMWuZ5g4.js";import{u as B}from"./header-_iNkTwr9.js";import{r as _,a as V,c as C,b as R,S as N}from"./checkIsFavorite-CDQXA8O7.js";import{a as U}from"./addToCart-DqmwY0nF.js";const X=e=>{var t;return`
+        <div id="${e.id}" class="col-lg-12">
+            <div class="product-content d-flex align-items-center gap-5">
+                <div class="img-container col-lg-4 bg-light p-4 rounded-16 overflow-hidden">
+                    <div class="variant-images splide">
+                        <div class="splide__track">
+                            <div class="splide__list">
+                                ${(t=e.variants)==null?void 0:t.map(r=>{var i;return(i=r.images)!=null&&i[0]?`
+                                        <div class="splide__slide mb-0">
+                                            <a href="./product.html?id=${e.id}&amp;variantId=${r.id}" class="d-block">
+                                                <img src="${r.images[0]}" class="d-block img-fluid mx-auto" />
+                                            </a>
+                                        </div>
+                                    `:""}).join("")}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="info flex-fill">
+                    <h6 class="mb-2">
+                        <a href="./product.html?id=${e.id}&amp;variantId=${e.variants[0].id}" class="product-title">${e.name}</a>
+                    </h6>
+                    <p class="fs-sm fw-semibold text-primary mb-3">${e.category}</p>
+                    <p class="fs-sm mb-2">${e.shortDescription}</p>
+                    <p class="fs-sm fw-medium text-gray-600 d-flex align-items-center gap-2 mb-2">
+                        <svg width="16" height="16" class="d-block text-warning">
+                            <use href="./src/assets/images/sprite.svg#starHalf"></use>
+                        </svg>
+                        <span>${e.rating} Rating</span>
+                    </p>
+                    <p class="price d-flex gap-1 fw-semibold">
+                        <span class="d-block">₹</span>
+                        <span class="d-block text-gray-600 text-decoration-line-through">${e.price.previous}.00</span>
+                        <span class="d-block fw-bold text-primary">${e.price.current}.00</span>
+                    </p>
+                    <div class="action d-flex gap-3 mt-4">
+                        <button class="add-to-favorite btn btn-sm text-primary p-2 d-flex align-items-center justify-content-center flex-shrink-0 border-primary rounded-circle" data-product-id="${e.id}" data-variant-id="${e.variants[0].id}">
+                            <svg width="18" height="18" class="d-block flex-shrink-0">
+                                <use href="./src/assets/images/sprite.svg#heart" />
+                            </svg>
+                        </button>
+                        <button class="add-to-cart btn btn-outline-primary btn-sm d-flex align-items-center justify-content-center gap-2" data-product-id="${e.id}" data-variant-id="${e.variants[0].id}">
+                            <svg width="18" height="18" class="d-block flex-shrink-0">
+                                <use href="./src/assets/images/sprite.svg#cart" />
+                            </svg>
+                            <span class="d-block">Add To Cart</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `},z=(e,t)=>{const r=typeof e=="string"?document.querySelector(e):e;if(!r)return;const i=()=>`
+        <div class="col-lg-12">
+            <div class="d-flex align-items-center gap-5">
+                <span class="placeholder col-lg-4 d-block p-4 rounded-16" style="height: 18.313rem;"></span>
+                <div class="flex-fill">
+                    <span class="placeholder d-block w-50 pb-4 mb-2"></span>
+                    <span class="placeholder d-block col-2 mb-3" style="height: 0.75rem;"></span>
+                    <div class="d-flex flex-column gap-2 mb-3">
+                        <span class="placeholder d-block w-100" style="height: 0.75rem;"></span>
+                        <span class="placeholder d-block w-25" style="height: 0.75rem;"></span>
+                    </div>
+                    <span class="placeholder d-block w-25 pb-4 mb-3 mb-4"></span>
+                    <div class="d-flex gap-3">
+                        <span class="placeholder d-block rounded-circle" style="width: 2.875rem;height: 2.875rem;"></span>
+                        <span class="placeholder d-block btn" style="width: 10.625rem;height: 2.875rem;"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `,a=document.createElement("div");a.className="row d-flex row-gap-4 px-0 mx-0",a.id="productListSkeleton";for(let d=0;d<t;d++)a.insertAdjacentHTML("beforeend",i());r.appendChild(a)},G=(e,t)=>{const r=typeof e=="string"?document.querySelector(e):e;if(!r)return;const i=()=>`
+        <span class="placeholder col-5 d-block rounded-32" style="height: 2.5rem;"></span>
+    `,a=document.createElement("li");a.className="w-100 d-flex gap-2 flex-wrap mb-0",a.id="categorySkeletonContainer";for(let d=0;d<t;d++)a.insertAdjacentHTML("beforeend",i());r.appendChild(a)},J=async e=>{G(e,5);try{const t=I(H,"collections","categories","items"),r=await M(t);if(r.empty){e.insertAdjacentHTML("beforeend",u("No Categories Found.","danger"));return}r.forEach(i=>{const a=i.data();e.insertAdjacentHTML("beforeend",`
+                <li class="mb-0">
+                    <label for="${a.id}" class="px-3 py-2 border rounded-32 transition-3 cursor-pointer">
+                        <input type="checkbox" id="${a.id}" name="${a.id}" data-category="${a.id}" class="category-checkbox d-none" />
+                        <span class="d-block">${a.name}</span>
+                    </label>
+                </li>
+            `)})}catch(t){console.log(t)}finally{const t=document.getElementById("categorySkeletonContainer");t&&t.remove()}},y=[];let x=!0;const l=document.getElementById("productsContainer"),T=document.getElementById("categoriesContainer"),m=document.getElementById("gridView"),h=document.getElementById("listView"),K=new URLSearchParams(window.location.search),b=K.get("category");b&&y.push(b);const L=async()=>{l.innerHTML="",x?_(l,9):z(l,9);try{let e=I(H,"collections","products","items");y.length>0&&(e=q(e,D("category","in",y)));const t=await M(e);if(t.empty){l.insertAdjacentHTML("beforeend",u("No Products Found."));return}t.forEach(r=>{const i=r.data();x?(l.setAttribute("data-layout","grid"),l.insertAdjacentHTML("beforeend",V(i))):(l.setAttribute("data-layout","list"),l.insertAdjacentHTML("beforeend",X(i)))}),requestAnimationFrame(()=>{(async()=>{const r=document.querySelectorAll(".variant-images"),i=document.querySelectorAll(".product-title"),a=document.querySelectorAll(".add-to-cart"),d=document.querySelectorAll(".add-to-favorite");if(a.forEach(s=>{s.addEventListener("click",g=>{g.preventDefault(),document.querySelectorAll(".alert").forEach(o=>o.remove()),s.disabled=!0,s.insertAdjacentHTML("beforeend",'<div class="spinner-border spinner-border-sm text-light" role="status"><span class="visually-hidden">Loading...</span></div>');const v=s.getAttribute("data-product-id"),c=s.getAttribute("data-variant-id");v&&c?U(v,c,1).then(o=>{o==="ADDED"?(s.parentElement.insertAdjacentHTML("afterend",u("Product Added To Cart","success","fs-xs")),B()):o==="EXISTS"&&s.parentElement.insertAdjacentHTML("afterend",u("Product Already Exist","danger","fs-xs"))}).catch(o=>{console.log("Failed to add to cart",o)}).finally(()=>{s.disabled=!1;const o=s.querySelector(".spinner-border");o&&o.remove()}):console.warn("Product or Variant ID missing")})}),d)for(const s of d){const g=s.getAttribute("data-product-id"),v=s.getAttribute("data-variant-id");try{await C(g,v)&&(s.classList.add("bg-primary","text-white"),s.innerHTML=`
+                                    <svg width="18" height="18" class="d-block flex-shrink-0">
+                                        <use href="./src/assets/images/sprite.svg#heartFill"></use>
+                                    </svg>
+                                `)}catch(c){console.log(c)}s.addEventListener("click",async c=>{c.preventDefault(),document.querySelectorAll(".alert").forEach(n=>n.remove()),s.disabled=!0,s.innerHTML='<div class="spinner-border spinner-border-sm text-primary" role="status"><span class="visually-hidden">Loading...</span></div>';const w=s.getAttribute("data-product-id"),o=s.getAttribute("data-variant-id");try{const n=await R({productId:w,variantId:o});n==="ADDED"?(s.parentElement.insertAdjacentHTML("afterend",u("Product Added To Favorites","success")),s.classList.add("bg-primary","text-white"),s.innerHTML=`
+                                        <svg width="18" height="18" class="d-block flex-shrink-0">
+                                            <use href="./src/assets/images/sprite.svg#heartFill"></use>
+                                        </svg>
+                                    `):n==="EXISTS"&&(s.parentElement.insertAdjacentHTML("afterend",u("Product Already Added To Favorites","danger")),s.innerHTML=`
+                                        <svg width="18" height="18" class="d-block flex-shrink-0">
+                                            <use href="./src/assets/images/sprite.svg#heartFill"></use>
+                                        </svg>
+                                    `)}catch(n){console.log(n)}finally{s.disabled=!1}})}r.forEach((s,g)=>{var o;new N(s,{perPage:1,arrows:!1,pagination:!0,drag:!1}).mount();const c=(o=t.docs[g])==null?void 0:o.data();if(!c||!c.variants)return;const w=s.querySelectorAll(".splide__pagination__page");c.variants.forEach((n,P)=>{const k=w[P];if(!k||!n.colors)return;const j=n.colors.map(p=>n.colors.length>1?`${p} ${100/n.colors.length}%`:p).join(", ");k.style.cssText=`
+                            --variant-base-color: ${n.colors[0]};
+                            background-image: linear-gradient( -45deg, ${j} );
+                            box-shadow: 0 0 0 0.125rem ${n.colors[0]};
+                        `,k.setAttribute("data-variant-id",`${n.id}`),k.addEventListener("click",()=>{const p=n.id,A=i[g],$=a[g],f=d[g];if(!A||!$)return;const S=new URL(A.href,window.location.origin);S.searchParams.delete("variantId"),S.searchParams.append("variantId",p),A.href=S.toString(),$.setAttribute("data-variant-id",`${p}`),f.setAttribute("data-variant-id",`${p}`);const F=f.getAttribute("data-product-id");C(F,p).then(E=>{E?(f.classList.add("bg-primary","text-white"),f.innerHTML=`
+                                        <svg width="18" height="18" class="d-block flex-shrink-0">
+                                            <use href="./src/assets/images/sprite.svg#heartFill"></use>
+                                        </svg>
+                                    `):(f.classList.remove("bg-primary","text-white"),f.innerHTML=`
+                                        <svg width="18" height="18" class="d-block flex-shrink-0">
+                                            <use href="./src/assets/images/sprite.svg#heart"></use>
+                                        </svg>
+                                    `)}).catch(E=>{console.log(E)})})})})})()})}catch(e){console.log(e),l.insertAdjacentHTML("beforeend",u("Error Loading Products."))}finally{const e=document.getElementById("productCardSkeleton"),t=document.getElementById("productListSkeleton");e&&e.remove(),t&&t.remove()}};L();T&&J(T).then(()=>{const e=T.querySelectorAll("input.category-checkbox");e.forEach(t=>{t.id===b&&(t.checked=!0,t.parentElement.classList.add("bg-primary","text-white")),t.addEventListener("change",r=>{r.preventDefault();const i=Array.from(e).filter(d=>d.checked).map(d=>d.getAttribute("data-category"));t.checked?t.parentElement.classList.add("bg-primary","text-white"):t.parentElement.classList.remove("bg-primary","text-white"),y.length=0,i.length>0&&y.push(...i);const a=new URL(window.location);b&&!i.includes(b)&&a.searchParams.delete("category"),window.history.replaceState({},"",a),L()})})});m&&m.addEventListener("click",e=>{e.preventDefault(),x=!0,L(),m.classList.remove("bg-light"),m.classList.add("bg-primary","text-white"),h&&(h.classList.remove("bg-primary","text-white"),h.classList.add("bg-light"))});h&&h.addEventListener("click",e=>{e.preventDefault(),x=!1,L(),h.classList.remove("bg-light"),h.classList.add("bg-primary","text-white"),m&&(m.classList.remove("bg-primary","text-white"),m.classList.add("bg-light"))});
